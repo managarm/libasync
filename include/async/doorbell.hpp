9895ff372939494cba31fa8c68dd9878d3ab7977
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2018-03-07 10:23:57 +0100

    doorbell: Refactor to use intrusive lists

--- include/async/doorbell.hpp
+++ include/async/doorbell.hpp
@@ -1,79 +1,116 @@
 #ifndef ASYNC_DOORBELL_HPP
 #define ASYNC_DOORBELL_HPP
 
-#include <vector>
 #include <async/result.hpp>
+#include <boost/intrusive/list.hpp>
 
 namespace async {
 
 namespace detail {
+	// Lock order: doorbell::_queue_mutex < node::_mutex
 	struct doorbell {
-		struct node : awaitable<void> {
-			node()
-			: _flags(0) { }
+		struct node : cancelable_awaitable<void>, boost::intrusive::list_base_hook<> {
+			node(doorbell *owner)
+			: _owner{owner}, _fired{false} { }
 
 			node(const node &) = delete;
 
 			node &operator= (const node &) = delete;
 
-			void trigger() {
-				auto f = _flags.fetch_or(has_value, std::memory_order_acq_rel);
-				assert(!(f & has_value));
-				if(f & has_awaiter) {
-					if(_awaiter)
-						_awaiter();
-					delete this;
-				}
+			void pretrigger() {
+				std::lock_guard<std::mutex> lock(_mutex);
+				_fired = true;
 			}
 
-			void then(callback<void()> awaiter) override {
-				_awaiter = awaiter;
+			void trigger() {
+				callback<void()> cb;
+				{
+					std::lock_guard<std::mutex> lock(_mutex);
 
-				auto f = _flags.fetch_or(has_awaiter, std::memory_order_acq_rel);
-				assert(!(f & has_awaiter));
-				if(f & has_value) {
-					_awaiter();
-					delete this;
+					cb = std::exchange(_cb, callback<void()>{});
 				}
+
+				if(cb)
+					cb();
+			}
+
+			void then(callback<void()> cb) override {
+				bool already_fired;
+				{
+					std::lock_guard<std::mutex> lock(_mutex);
+					assert(!_cb);
+
+					already_fired = _fired;
+					if(!already_fired)
+						_cb = cb;
+				}
+
+				if(already_fired)
+					cb();
+			}
+
+			void cancel() override {
+				callback<void()> cb;
+				{
+					std::lock_guard<std::mutex> queue_lock(_owner->_queue_mutex);
+					std::lock_guard<std::mutex> lock(_mutex);
+					
+					if(_fired)
+						return;
+
+					auto it = boost::intrusive::list<node>::s_iterator_to(*this);
+					_owner->_queue.erase(it);
+					cb = std::exchange(_cb, callback<void()>{});
+					_fired = true;
+				}
+
+				if(cb)
+					cb();
 			}
 
 			void detach() override {
-				auto f = _flags.fetch_or(has_awaiter, std::memory_order_acq_rel);
-				assert(!(f & has_awaiter));
-				if(f & has_value)
-					delete this;
+				assert(_fired); // This assumption is false if we use cancel().
+				// We inspect _cb without holding the lock.
+				// We someone changes it concurrently, the contract is broken anyway.
+				assert(!_cb);
+				delete this;
 			}
 
 		private:
-			std::atomic<int> _flags;
-			callback<void()> _awaiter;
+			doorbell *_owner;
+
+			std::mutex _mutex;
+			bool _fired;
+			callback<void()> _cb;
 		};
 
 		void ring() {
-			std::vector<node *> items;
+			boost::intrusive::list<node> items;
 			{
-				std::lock_guard<std::mutex> lock(_mutex);
-				items = std::move(_items);
-				_items.clear();
+				std::lock_guard<std::mutex> queue_lock(_queue_mutex);
+
+				// Grab all items and mark them as retired while we hold the lock.
+				items.splice(items.end(), _queue);
+				for(auto &ref : items)
+					ref.pretrigger();
 			}
 
-			for(auto item : items)
-				item->trigger();
+			for(auto &ref : items)
+				ref.trigger();
 		}
 
-		result<void> async_wait() {
-			auto item = new node;
+		cancelable_result<void> async_wait() {
+			auto item = new node{this};
 			{
-				std::lock_guard<std::mutex> lock(_mutex);
-				_items.push_back(item);
+				std::lock_guard<std::mutex> queue_lock(_queue_mutex);
+				_queue.push_back(*item);
 			}
-			return result<void>{item};
+			return cancelable_result<void>{item};
 		}
 
 	private:
-		std::mutex _mutex;
-		// TODO: Make this list intrusive.
-		std::vector<node *> _items;
+		std::mutex _queue_mutex;
+		boost::intrusive::list<node> _queue;
 	};
 }
 
